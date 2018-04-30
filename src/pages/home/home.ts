732e7c743414019e25e9bf600096d14d1956bfbb
diku1968 <diku1968@gmail.com>
2018-04-30 16:40:00 +0530

    Your Awesome Changes!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  public items:any;
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+  this.loadData();
   }
 
+  loadData(){
+    let data:Observable<any>
+    data= this.http.get('http://182.19.82.186:94/api/mobtrans');
+    data.subscribe(result=> {
+      this.items = result;
+    })
+  }
+  itemClick(itemmobile:number){
+    alert(itemmobile);
+  }
 }
